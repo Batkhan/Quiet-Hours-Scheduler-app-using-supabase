4be626fb5571efcb7504992633be1b70e44d5731
Batkhan <hamdanikutty@gmail.com>
2025-09-21 01:49:28 +0530

    some updates

--- scripts/runSendQuietHourEmails.js
+++ scripts/runSendQuietHourEmails.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 
 const secret = process.env.CRON_SECRET;
-const url = `https://quiet-hours-scheduler-app-using-supabase-q4tnp3hqj.vercel.app/api/sendQuietHourEmails?secret=${secret}`;
+const url = `https://quiet-hours-scheduler-app-using-supabase-5bndgoafv.vercel.app/api/sendQuietHourEmails?secret=${secret}`;
 
 const res = await fetch(url);
 const data = await res.json();
